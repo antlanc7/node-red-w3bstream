0d34ca0cc24db4ea3a2228a5afed58f3ed469971
Antonio <antonio.lanciotti@live.it>
2023-09-12 15:39:58 +0200

    initial commit

---
+++ w3bstream.js
@@ -0,0 +1,35 @@
+module.exports = function (RED) {
+  function W3bstreamNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    (async () => {
+      const { W3bstreamClient } = await import("w3bstream-client-js");
+      try {
+        node.client = new W3bstreamClient(config.url, config.apikey);
+      } catch (error) {
+        node.error(error);
+      }
+    })();
+    node.on("input", function (msg, send, done) {
+      send =
+        send ||
+        function () {
+          node.send.apply(node, arguments);
+        };
+      node.client
+        .publishSingle(
+          {
+            device_id: config.deviceid,
+            event_type: config.eventtype || msg.topic,
+          },
+          msg.payload
+        )
+        .then((res) => {
+          msg.payload = res.data;
+          send(msg);
+          done?.();
+        });
+    });
+  }
+  RED.nodes.registerType("w3bstream", W3bstreamNode);
+};
